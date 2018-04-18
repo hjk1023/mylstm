e20abd71f861f061e947c3c2ce929d6b1725ed97
HuangJK <854817033@qq.com>
2018-04-18 15:56:27 +0800

    0

--- src/Demo.java
+++ src/Demo.java
@@ -8,7 +8,7 @@ public class Demo {
         m1.print(3,2);
         Matrix m2 = m1.plus(m1);
         m2.print(6,4);
-        Matrix m3 = new Matrix(3,3);
+        Matrix m3 = new Matrix(3,4);
         m3.print(6,2);
 
     }
