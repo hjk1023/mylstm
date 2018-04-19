20f7a44403270ccade1dd19f4a74293bb04a0cc1
HuangJK <854817033@qq.com>
2018-04-20 00:59:52 +0800

    0

--- src/Demo.java
+++ src/Demo.java
@@ -1,15 +1,22 @@
-import Jama.Matrix;
+import com.hjk.matrix.Matrix;
 
+//import com.hjk.lstm.Lstm;
 public class Demo {
     public static void main(String[] args)
     {
         double[][] array = {{1,2,3},{4,5,6}};
         Matrix m1 = new Matrix(array);
+        Matrix m2;
         m1.print(3,2);
-        Matrix m2 = m1.plus(m1);
-        m2.print(6,4);
-        Matrix m3 = new Matrix(3,4);
-        m3.print(6,2);
+        //Lstm lstm = new Lstm();
+        m2 = Matrix.exp(m1);
+        m2.print(10, 8);
+
+        //m1 = lstm.tanh(m1);
+        m1.print(10,8);
+
+
+
 
     }
 }
